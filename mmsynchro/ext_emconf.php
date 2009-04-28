e2e7438b6c7aecce3099463464a909ad0bbe78db
t!m[] <t!m[]@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-28 09:14:17 +0000

    Bugfixes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mmsynchro/trunk@19594 735d13b6-9817-0410-8766-e36946ffe9aa

--- mmsynchro/ext_emconf.php
+++ mmsynchro/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "mmsynchro"
 #
-# Auto generated 09-09-2008 12:01
+# Auto generated 28-04-2009 11:15
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Synchronisation',
 	'description' => '',
 	'category' => 'module',
-	'author' => 'Timo Webler',
-	'author_email' => 'timo.webler@mediaman.de',
+	'author' => 'Patrick Kaeller, Timo Webler',
+	'author_email' => 'kaeller@mediaman.de',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => 'L',
-	'author_company' => 'mediaman Gesellschaft für Kommunikation mbH',
-	'version' => '1.5.2',
+	'author_company' => 'mediaman Gesellschaft fuer Kommunikation mbH',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.6-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"9f2d";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"8210";s:17:"ext_localconf.php";s:4:"ccb8";s:14:"ext_tables.php";s:4:"b3d7";s:14:"ext_tables.sql";s:4:"888b";s:7:"tca.php";s:4:"d251";s:30:"cli/class.tx_mmsynchro_cli.php";s:4:"b298";s:14:"doc/manual.sxw";s:4:"07cc";s:19:"doc/wizard_form.dat";s:4:"0846";s:20:"doc/wizard_form.html";s:4:"4ea3";s:16:"images/Thumbs.db";s:4:"80ae";s:26:"images/img_0003_mmlogo.gif";s:4:"9052";s:25:"images/img_mm_logo_02.gif";s:4:"f4bd";s:18:"Lang/locallang.xml";s:4:"6d50";s:25:"Lang/locallang_csh_db.xml";s:4:"294d";s:21:"Lang/locallang_db.xml";s:4:"e3f7";s:23:"Lang/locallang_mod1.xml";s:4:"ee2d";s:36:"lib/class.tx_mmsynchro_connectDB.php";s:4:"9392";s:32:"lib/class.tx_mmsynchro_crypt.php";s:4:"638e";s:40:"lib/class.tx_mmsynchro_dbalConnectDB.php";s:4:"2919";s:40:"lib/class.tx_mmsynchro_localPageTree.php";s:4:"176f";s:38:"lib/class.tx_mmsynchro_progressBar.php";s:4:"334c";s:33:"lib/class.tx_mmsynchro_remote.php";s:4:"10cb";s:34:"lib/class.tx_mmsynchro_synchro.php";s:4:"19e3";s:40:"lib/class.tx_mmsynchro_tcemainprocdm.php";s:4:"1bde";s:39:"lib/class.tx_mmsynchro_validatePath.php";s:4:"0a4d";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"c5b5";s:14:"mod1/index.php";s:4:"16ae";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+	'_md5_values_when_last_written' => 'a:54:{s:9:"ChangeLog";s:4:"9f2d";s:10:"README.txt";s:4:"9fa9";s:21:"ext_conf_template.txt";s:4:"6f4b";s:12:"ext_icon.gif";s:4:"8210";s:17:"ext_localconf.php";s:4:"7626";s:14:"ext_tables.php";s:4:"ac24";s:14:"ext_tables.sql";s:4:"97b9";s:7:"tca.php";s:4:"747d";s:30:"cli/class.tx_mmsynchro_cli.php";s:4:"1b50";s:13:"cm1/clear.gif";s:4:"cc11";s:15:"cm1/cm_icon.gif";s:4:"8074";s:12:"cm1/conf.php";s:4:"72d8";s:13:"cm1/index.php";s:4:"ec41";s:17:"doc/de_manual.sxw";s:4:"ccbe";s:16:"images/Thumbs.db";s:4:"80ae";s:26:"images/img_0003_mmlogo.gif";s:4:"9052";s:25:"images/img_mm_logo_02.gif";s:4:"f4bd";s:18:"Lang/locallang.xml";s:4:"3ab6";s:22:"Lang/locallang_cm1.xml";s:4:"5ac0";s:25:"Lang/locallang_csh_db.xml";s:4:"2c87";s:21:"Lang/locallang_db.xml";s:4:"327b";s:23:"Lang/locallang_mod1.xml";s:4:"e2c0";s:23:"Lang/locallang_mod2.xml";s:4:"1b52";s:23:"Lang/locallang_mod3.xml";s:4:"9806";s:23:"Lang/locallang_mod4.xml";s:4:"a997";s:24:"Lang/de/de_locallang.xml";s:4:"9dd5";s:28:"Lang/de/de_locallang_cm1.xml";s:4:"4890";s:31:"Lang/de/de_locallang_csh_db.xml";s:4:"add2";s:27:"Lang/de/de_locallang_db.xml";s:4:"7526";s:29:"Lang/de/de_locallang_mod1.xml";s:4:"9a72";s:29:"Lang/de/de_locallang_mod2.xml";s:4:"4b6b";s:29:"Lang/de/de_locallang_mod3.xml";s:4:"e7b0";s:29:"Lang/de/de_locallang_mod4.xml";s:4:"940f";s:33:"lib/class.tx_mmsynchro_backup.php";s:4:"c48c";s:30:"lib/class.tx_mmsynchro_cm1.php";s:4:"6d5d";s:36:"lib/class.tx_mmsynchro_connectDB.php";s:4:"2d05";s:40:"lib/class.tx_mmsynchro_dbalConnectDB.php";s:4:"b406";s:40:"lib/class.tx_mmsynchro_localPageTree.php";s:4:"619d";s:38:"lib/class.tx_mmsynchro_progressBar.php";s:4:"334c";s:33:"lib/class.tx_mmsynchro_remote.php";s:4:"b4fe";s:34:"lib/class.tx_mmsynchro_synchro.php";s:4:"418f";s:39:"lib/class.tx_mmsynchro_validatePath.php";s:4:"c725";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"ed82";s:14:"mod1/index.php";s:4:"d057";s:19:"mod1/moduleicon.gif";s:4:"8074";s:14:"mod2/clear.gif";s:4:"cc11";s:13:"mod2/conf.php";s:4:"86ce";s:14:"mod2/index.php";s:4:"890a";s:19:"mod2/moduleicon.gif";s:4:"8074";s:14:"mod3/clear.gif";s:4:"cc11";s:13:"mod3/conf.php";s:4:"5bef";s:14:"mod3/index.php";s:4:"e87a";s:19:"mod3/moduleicon.gif";s:4:"8074";}',
 	'suggests' => array(
 	),
 );
